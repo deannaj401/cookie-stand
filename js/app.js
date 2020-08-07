d101ec2a045a53a610da6c56d77399005a644b6b
Deanna Johnson <deannaj401@gmail.com>
2020-08-06 21:59:06 -0600

    starting project

---
+++ js/app.js
@@ -0,0 +1,38 @@
+/* eslint-disable indent */
+'use strict';
+
+var seattle = {
+    min: 23,
+    max: 65,
+    average: 6.3
+};
+console.log('seattle = ' + seattle);
+
+
+var tokyo = {
+    min: 3,
+    max: 24,
+    average: 1.2
+};
+console.log('tokyo = ' + tokyo);
+
+var dubai = {
+    min: 11,
+    max: 38,
+    average: 3.7
+};
+console.log('dubai = ' + dubai);
+
+var paris = {
+    min: 20,
+    max: 38,
+    average: 2.3
+};
+console.log('paris = ' + paris);
+
+var lima = {
+    min: 2,
+    max: 16,
+    average: 4.6
+};
+console.log('lima = ' + lima);
